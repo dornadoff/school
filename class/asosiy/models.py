257d0526392e1ac6c154b94db5cbc86cf549faa7
dornadoff <dornadoff@gmail.com>
2023-05-17 18:55:19 +0500

    Shool

--- class/asosiy/models.py
+++ class/asosiy/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    date = models.DateField()
+
+class Subject(models.Model):
+    title = models.CharField(max_length=100)
+    clas = models.SmallIntegerField()
+
 class Img(models.Model):
     name = models.CharField(max_length=100)
     img = models.FileField(upload_to="ava", null=True)
@@ -8,14 +18,23 @@ class Img(models.Model):
         return self.name
 
 class Student(models.Model):
-    i_f_o = models.CharField(max_length=100)
+    fullname = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     clas = models.SmallIntegerField()
     img = models.ForeignKey(Img, on_delete=models.CASCADE, null=True)
     number = models.CharField(max_length=14)
     social_network = models.CharField(max_length=1000)
+    school = models.ForeignKey(School, on_delete=models.CASCADE, null=True)
+    date_of_bearth = models.DateField(null=True)
+
     def __str__(self):
-        return self.i_f_o
+        return self.fullname
+
+class Journal(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    mark = models.SmallIntegerField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date_time = models.DateTimeField(auto_now_add=True)
 
 class InfoStudent(models.Model):
     info = models.TextField()
@@ -23,3 +42,27 @@ class InfoStudent(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.info
+
+
+class Teacher(models.Model):
+    fullname = models.CharField(max_length=100)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    img = models.ForeignKey(Img, on_delete=models.CASCADE)
+
+class Clas(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    number = models.SmallIntegerField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student, related_name="classes")
+
+class StudyTime(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    start_lesson = models.TimeField()
+    end_lesson = models.TimeField()
+    clas = models.ForeignKey(Clas, on_delete=models.CASCADE)
+
+
+
+
